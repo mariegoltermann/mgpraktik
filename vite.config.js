06365f27ea8a495273749b08ea50237028c9a01a
mariegoltermann <mari09a7@stud.kea.dk>
2022-10-24 11:20:59 +0200

    df

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+      about: resolve(__dirname, "src/portfolio.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
